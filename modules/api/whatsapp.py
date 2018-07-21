d0db45b8811f014106f12994f80a61a9494c6813
Alexis R. M <agl125@hotmail.com>
2018-07-20 21:11:53 -0500

    [1.0][feature] receiving message on whatsapp.py

--- modules/api/whatsapp.py
+++ modules/api/whatsapp.py
@@ -5,22 +5,7 @@ Signals this module listents to:
 1. When a message is received (signals.command_received)
 ==========================================================
 '''
-@signals.command_received.connect
+@signals.message_received.connect
 def handle(message):
-    if message.command == "hi":
-        hi(message)
-    elif message.command == "help":
-        help(message)
-
-'''
-Actual module code
-==========================================================
-'''
-def hi(message):
-    who_name = message.who_name
-    answer = "Hi " + who_name
-    mac.send_message(answer, message.conversation)
-
-def help(message):
-    answer = "*Bot called mac* \nWhatsapp framework made in Python \n*Version:* 1.0.0 \n*Status:* Beta \nhttps://github.com/danielcardeenas/whatsapp-framework"
-    mac.send_message(answer, message.conversation)
+    print(message)
+    mac.send_message("recibido", message.conversation)
